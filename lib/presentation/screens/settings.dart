c8ca588eece77cd5b0f882f214221156a3fb417c
ShubhamHande99 <shubhamhandebgm@gmail.com>
2024-01-04 18:05:18 +0530

    feature : Add support for l18n and l10n

--- lib/presentation/screens/settings.dart
+++ lib/presentation/screens/settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_l18n/core/utils/utils.dart';
+import 'package:flutter_l18n/main.dart';
+import 'package:flutter_l18n/presentation/screens/select_language.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -8,13 +11,22 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  String getLanguageName(String locale) {
+    for (Map language in languages) {
+      if (language['locale'] == locale) {
+        return language['name'];
+      }
+    }
+    return 'en'; // Return default en as fallback value
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal,
         title: Text(
-          'Settings',
+          AppLocalization.of(context)!.settings,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: Colors.white,
               ),
@@ -94,47 +106,54 @@ class _SettingsState extends State<Settings> {
       children: [
         optionListItem(
           icon: Icons.key_sharp,
-          title: 'Account',
-          sunTitle: 'Security notifications, change number',
+          title: AppLocalization.of(context)!.account,
+          sunTitle: AppLocalization.of(context)!.accountSubtitle,
         ),
         optionListItem(
           icon: Icons.lock,
-          title: 'Privacy',
-          sunTitle: 'Block contacts, disappearing messages',
+          title: AppLocalization.of(context)!.privacy,
+          sunTitle: AppLocalization.of(context)!.privacySubtitle,
         ),
         optionListItem(
           icon: Icons.lock,
-          title: 'Avatar',
-          sunTitle: 'Create, edit, profile photo',
+          title: AppLocalization.of(context)!.avatar,
+          sunTitle: AppLocalization.of(context)!.avatarSubtitle,
         ),
         optionListItem(
           icon: Icons.chat_sharp,
-          title: 'Chats',
-          sunTitle: 'Theme, wallpapers, chat history',
+          title: AppLocalization.of(context)!.chats,
+          sunTitle: AppLocalization.of(context)!.chatsSubtitle,
         ),
         optionListItem(
           icon: Icons.notification_important,
-          title: 'Notifications',
-          sunTitle: 'Message, group & call tones',
+          title: AppLocalization.of(context)!.notifications,
+          sunTitle: AppLocalization.of(context)!.notificationsSubtitle,
         ),
         optionListItem(
           icon: Icons.settings_backup_restore_sharp,
-          title: 'Storage and data',
-          sunTitle: 'Network usage, auto-download',
+          title: AppLocalization.of(context)!.storageAndData,
+          sunTitle: AppLocalization.of(context)!.storageAndDataSubtitle,
         ),
         optionListItem(
           icon: Icons.language,
-          title: 'App language',
-          sunTitle: 'English',
+          title: AppLocalization.of(context)!.appLanguage,
+          sunTitle: getLanguageName(languageNotifier.value),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const SelectLanguage(),
+              ),
+            );
+          },
         ),
         optionListItem(
           icon: Icons.help,
-          title: 'Help',
-          sunTitle: 'Help center, contact us, privacy policy',
+          title: AppLocalization.of(context)!.help,
+          sunTitle: AppLocalization.of(context)!.helpSubtitle,
         ),
         optionListItem(
           icon: Icons.person_pin_outlined,
-          title: 'Invite a friend',
+          title: AppLocalization.of(context)!.inviteAFriend,
         ),
       ],
     );
@@ -143,21 +162,24 @@ class _SettingsState extends State<Settings> {
   Widget optionListItem({
     required IconData icon,
     required String title,
-     String? sunTitle,
+    String? sunTitle,
+    void Function()? onTap,
   }) {
     return ListTile(
-      onTap: (){},
+      onTap: onTap,
       leading: Icon(
         icon,
         color: Colors.grey,
       ),
       title: Text(title),
-      subtitle: sunTitle!=null ?  Text(
-        sunTitle,
-        style: Theme.of(context).textTheme.bodySmall!.copyWith(
-              color: Colors.grey,
-            ),
-      ) : null,
+      subtitle: sunTitle != null
+          ? Text(
+              sunTitle,
+              style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Colors.grey,
+                  ),
+            )
+          : null,
     );
   }
 }
